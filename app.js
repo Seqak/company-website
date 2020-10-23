ea0aee306a1e42b0600ddcc14656e380b3405ed2
Kacper Kozioł <kacperu770@gmail.com>
2020-10-23 17:45:20 +0200

    Home page done

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
 const tl = gsap.timeline({defaults: {ease: 'power1.out'}});
 
-tl.to(".g-sap", {x: "0%", duration: 0.5, stagger: 0.05});
+tl.to(".g-sap", {x: "0%", duration: 1});
+
+tl.to(".g-sap-btn", {y: "0%", duration: 0.3})
