4a9a9509856e8bb61ef7cd62a740f227931b1d50
Kacper Kozioł <kacperu770@gmail.com>
2020-10-24 14:45:53 +0200

    Added Portfolio page

--- app.js
+++ app.js
@@ -1,16 +1,16 @@
 const tl = gsap.timeline({defaults: {ease: 'power1.out'}});
-const tlTwo = gsap.timeline({defaults: {ease: 'SlowMo.out'}});
+const tlTwo = gsap.timeline({defaults: {ease: 'power2.out'}});
+const tlSlowMo = gsap.timeline({defaults: {ease: 'SlowMo.out'}});
 
 tl.to(".g-sap", {x: "0%", duration: 1, opacity:1});
 
 tl.to(".g-sap-btn", {y: "0%", duration: 0.3, opacity:1})
 
-tlTwo.to("#about-title", {y: "0%", duration: 0.5, opacity:1})
+tlSlowMo.to("#about-title", {y: "0%", duration: 0.5, opacity:1})
 
-tlTwo.to(".about-content", {x: "0%", duration: 0.5, opacity:1})
+tlSlowMo.to(".about-content", {x: "0%", duration: 0.5, opacity:1})
 
-tlTwo.to(".header-img", {x: "0%", duration: 1, opacity:1})
+tlSlowMo.to(".header-img", {x: "0%", duration: 1, opacity:1})
 
 
-
-// NEWSLETTER confirmation
+tlTwo.to(".project-box img, .project-box h2, .project-box p", {x: "0%", duration: 1, opacity:1})
