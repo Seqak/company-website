3dd110515ac1ebc00a6bd2477518f160c1a9f0b7
Kacper Kozioł <kacperu770@gmail.com>
2020-10-24 13:30:01 +0200

    Added success modal after sign up to the newsletter

--- app.js
+++ app.js
@@ -10,3 +10,7 @@ tlTwo.to("#about-title", {y: "0%", duration: 0.5, opacity:1})
 tlTwo.to(".about-content", {x: "0%", duration: 0.5, opacity:1})
 
 tlTwo.to(".header-img", {x: "0%", duration: 1, opacity:1})
+
+
+
+// NEWSLETTER confirmation
