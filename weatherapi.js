5d38d4630482997e7927ecec31bcee41127c3153
Kacper Kozioł <kacperu770@gmail.com>
2021-01-13 21:55:02 +0100

    test

--- weatherapi.js
+++ weatherapi.js
@@ -10,17 +10,12 @@ $.ajax('http://ip-api.com/json').done(function (cityResponse) {
         localStorage.setItem('temp', weatherData.main["temp"]);
         var guestTemp =myStorage.getItem('temp');
 
-    console.log("Info:");
-        console.log(guestTemp);
-        console.log(guestCity);
 
-        var guestCity =myStorage.getItem('city');
-        var guestTemp =myStorage.getItem('temp');
         $("#yourTemp").html(guestCity + ': '+ guestTemp +'&deg;C');
     });
 });
 
-var guestCity =myStorage.getItem('city');
+// var guestCity =myStorage.getItem('city');
 // $.ajax('https://api.openweathermap.org/data/2.5/weather?q='+guestCity +'&units=metric&appid=cf833ea4d9b83ed6d740d22700d79d1b').done(function (weatherResponse) {
 //     weatherData = weatherResponse;
 //     localStorage.setItem('temp', weatherData.main["temp"]);
@@ -31,6 +26,6 @@ var guestCity =myStorage.getItem('city');
 //     console.log(guestCity);
 // });
 
-var guestCity =myStorage.getItem('city');
-var guestTemp =myStorage.getItem('temp');
-$("#yourTemp").html(guestCity + ': '+ guestTemp +'&deg;C');
+// var guestCity =myStorage.getItem('city');
+// var guestTemp =myStorage.getItem('temp');
+// $("#yourTemp").html(guestCity + ': '+ guestTemp +'&deg;C');
