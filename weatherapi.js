3918241f0e47b7a0cb871d6daf12fea376955879
Kacper Kozioł <kacperu770@gmail.com>
2021-01-17 09:42:16 +0100

    Margins corrected

--- weatherapi.js
+++ weatherapi.js
@@ -14,18 +14,3 @@ $.ajax('http://ip-api.com/json').done(function (cityResponse) {
         $("#yourTemp").html(guestCity + ': '+ guestTemp +'&deg;C');
     });
 });
-
-// var guestCity =myStorage.getItem('city');
-// $.ajax('https://api.openweathermap.org/data/2.5/weather?q='+guestCity +'&units=metric&appid=cf833ea4d9b83ed6d740d22700d79d1b').done(function (weatherResponse) {
-//     weatherData = weatherResponse;
-//     localStorage.setItem('temp', weatherData.main["temp"]);
-//     var guestTemp =myStorage.getItem('temp');
-//
-// console.log("Info:");
-//     console.log(guestTemp);
-//     console.log(guestCity);
-// });
-
-// var guestCity =myStorage.getItem('city');
-// var guestTemp =myStorage.getItem('temp');
-// $("#yourTemp").html(guestCity + ': '+ guestTemp +'&deg;C');
